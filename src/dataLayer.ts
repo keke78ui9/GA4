8ae05e8adb1beb3964d8e2f02d58b355982b2e00
keke <keke78ui9@outlook.com>
2023-05-01 19:15:30 -0700

    add gtag types

--- src/dataLayer.ts
+++ src/dataLayer.ts
@@ -1,11 +1,11 @@
 function get(key:string, getLast?:boolean):string | object {
     if (window.dataLayer) {
-        // if (getLast) {
-        //     const data = window.dataLayer?.find((item) => item[key]);    
-        // }
-
-        const data = window.dataLayer?.find((item) => item[key]);
+        const data = getLast 
+        ? window.dataLayer?.findLast((item) => item[key])
+        : window.dataLayer?.find((item) => item[key]);
         return data ? data[key] : '';
     }
     return '';
 }
+
+export {get};
